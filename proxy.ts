dfde5547104f97aa3658583a4b3e1f8f436062c0
17anirudh <vedalaanirudh@gmail.com>
2026-04-05 10:14:23 +0530

    added basic hashing to not save user's raw ip

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,7 @@
 import { Redis } from '@upstash/redis'
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import crypto from 'crypto'
 
 const redis = Redis.fromEnv()
 export const config = { matcher: '/' }
@@ -10,8 +11,15 @@ export const config = { matcher: '/' }
 export default async function proxy(req: NextRequest) {
     if (new URL(req.url).pathname !== '/') return NextResponse.next();
 
-    const ip = req.headers.get('x-forwarded-for')?.split(',')[0] || '127.0.0.1';
-    const fingerPrint = `v:${ip}:${new Date().getUTCDate()}`;
+    const ip = req.headers.get("x-forwarded-for")?.split(",")[0].trim() || "127.0.0.1";
+    const date = new Date().toISOString().slice(0, 10);
+
+    const hash = crypto
+                    .createHash("sha1")
+                    .update(`${ip}:${date}`)
+                    .digest("hex");
+
+    const fingerPrint = `v:${hash}`;
 
     redis.set(fingerPrint, "1", { nx: true, ex: 86400 }).then((isNew) => {
         if (isNew) redis.incr("portfolio_visits");
